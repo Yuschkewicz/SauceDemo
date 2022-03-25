8a0c164318b87a80596a03bf0f4a76c8a86f7d6e
Yuschkewicz <13and.jei@gmail.com>
2022-03-25 19:46:16 +0300

    0.8

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class CheckoutTest extends BaseTest {
 
-    @Test(description = "Проверка функциональности покупки товара",retryAnalyzer = Retry.class)
+    @Test(description = "Проверка функциональности покупки товара", retryAnalyzer = Retry.class)
     public void checkOutTwoItems() {
         loginPage.registration();
         productsPage.chooseThreeItemAndChangeOnTwo();
